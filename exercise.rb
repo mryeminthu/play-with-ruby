04322fa14ac3303ff6168106f9fba6d77046e6b9
mryeminthu <dhammaarchive@hotmail.com>
2023-10-29 19:50:28 -0700

    Add basic exercise

---
+++ exercise.rb
@@ -0,0 +1,13 @@
+puts 2 + 2
+puts 10 / 3
+puts 10 / 3.0
+puts "Happy" + "coding"
+puts "Happy" + "\n" + "coding"
+print "Happy" + "\n" + "coding" + "\n"
+
+a = 3
+puts a
+a = "Hello"
+puts a
+b = "World!"
+puts a + b
